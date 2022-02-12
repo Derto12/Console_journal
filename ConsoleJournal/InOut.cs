98de10310be40590866ef4d0705153139dd5f548
Balint Erdosi <3rdosi.balint@gmail.com>
2022-02-12 14:37:23 +0100

    Create InOut class - handles actions featuring basic input / output (e.g. write colored text, write text at given location, remove text)

---
+++ ConsoleJournal/InOut.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleJournal 
+{
+    public class InOut 
+    {
+        public static void Write(string msg, ConsoleColor foreColor, ConsoleColor backColor = ConsoleColor.Black) {
+            Console.ForegroundColor = foreColor;
+            Console.BackgroundColor = backColor;
+            Console.Write(msg);
+            Console.ResetColor();
+        }
+
+        public static void Write(string msg, Location loc) {
+            Console.SetCursorPosition(loc.left, loc.top);
+            Console.Write(msg);
+        }
+
+        public static void Write(string msg, Location loc, ConsoleColor foreColor, ConsoleColor backColor = ConsoleColor.Black) {
+            Console.SetCursorPosition(loc.left, loc.top);
+            Write(msg, foreColor, backColor);
+        }
+
+        public static void ClearBehind() {
+            int origLeft = Console.CursorLeft;
+            for (int left = origLeft; left < Console.BufferWidth; left++) {
+                Console.CursorLeft = left;
+                Console.Write(" ");
+            }
+            Console.CursorLeft = origLeft;
+        }
+
+        public static void ClearLine(int top) {
+            Console.SetCursorPosition(0, top);
+            ClearBehind();
+        }
+    }
+}
