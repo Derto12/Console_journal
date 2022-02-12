7359642351b073989e3cc9d661a9d3682736b05f
Balint Erdosi <3rdosi.balint@gmail.com>
2022-02-12 14:36:59 +0100

    Create Location struct - for storing console locations Remove unnecessary code from Program.cs

---
+++ ConsoleJournal/Location.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleJournal 
+{
+    public struct Location {
+        public int left;
+        public int top;
+
+        public override string ToString() => $"({left};{top})";
+    }
+}
