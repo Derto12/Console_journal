8a8928ebe9a9ef07d02f5ca3aa2fdbc2645d62c9
Balint Erdosi <3rdosi.balint@gmail.com>
2022-03-02 20:54:38 +0100

    Created basic mainmenu

--- ConsoleJournal/Program.cs
+++ ConsoleJournal/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Figgle;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,80 @@ namespace ConsoleJournal
 {
     class Program
     {
+        static Screen currentScreen = Screen.Main;
+
+        static void MainMenu()
+        {
+            Console.Clear();
+
+            var banner = $"{FiggleFonts.Standard.Render("Journal")}{FiggleFonts.Standard.Render("is Life!")}";
+            InOut.PrintLinesAt(banner, new Location(0, 5), InOut.TextAlign.Center);
+
+            Console.SetCursorPosition(3, 21);
+
+            string choice = Menu.SelectItem(new string[] { "New journal", "View documents", "Settings", "Exit" });
+            switch(choice) 
+            {
+                case "New journal":
+                    //currentScreen = Screen.New;
+                    break;
+                case "View documents":
+                    //currentScreen = Screen.Docs;
+                    break;
+                case "Settings":
+                    //currentScreen = Screen.Settings;
+                    break;
+                case "Exit":
+                    currentScreen = Screen.None;
+                    break;
+            }
+        }
+
+        static void NewMenu()
+        {
+
+        }
+
+        static void DocsMenu()
+        {
+
+        }
+
+        static void SettingsMenu()
+        {
+
+        }
+
+        static void Initialize()
+        {
+            
+        }
+
+
         static void Main(string[] args)
         {
+            bool exit = false;
+            while (!exit)
+            {
+                switch (currentScreen)
+                {
+                    case Screen.Main:
+                        MainMenu();
+                        break;
+                    case Screen.New:
+                        NewMenu();
+                        break;
+                    case Screen.Docs:
+                        DocsMenu();
+                        break;
+                    case Screen.Settings:
+                        SettingsMenu();
+                        break;
+                    case Screen.None:
+                        exit = true;
+                        break;
+                }
+            }
         }
     }
 }
